9fc87c36184927067e79194c53555889c98df44d
TARUN <125533961+tarunmnc26@users.noreply.github.com>
2026-06-07 01:43:34 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>> mp;
+        for(string s : strs){
+            string t=s;
+            sort(t.begin(),t.end());
+            mp[t].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+
+        for(auto &it : mp){
+            ans.push_back(it.second);
+        }
+
+        return ans;
+    }
+};
---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int,int> freq;
+        for(int num:nums){
+            freq[num]++;
+        }
+        multimap<int,int> mp;
+        for(const auto& it: freq){
+            mp.insert({it.second,it.first});
+        }
+        vector<int> ans;
+        auto it=mp.rbegin();
+        for(int i=0;i<k;i++){
+            ans.push_back(it->second);
+            ++it;
+        }
+        return ans;
+    }
+};
